a60714d8a0a914d5785e551884d3aa3676315ca0
tommysgithub23 <147836570+tommysgithub23@users.noreply.github.com>
2025-01-03 13:13:45 +0100

    automatisches berechnen von d_P, V_P, Plot-Tab, Error Raise bei falscher Eingabe

--- gui/single_layer_gui.py
+++ gui/single_layer_gui.py
@@ -2,11 +2,14 @@ from PySide6.QtWidgets import (
     QWidget, QVBoxLayout, QTabWidget, QHBoxLayout, QFormLayout, QLineEdit,
     QLabel, QCheckBox, QComboBox, QGraphicsView, QGraphicsScene, QGraphicsRectItem, QPushButton, QSpacerItem, QSizePolicy
 )
+from PySide6.QtGui import QPalette, QColor
 from PySide6.QtCore import Qt
-from sl_model_functions import migrationsmodell_piringer, calculate_max_cp0, plot_results_area
-import os
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 import numpy as np
+import os
 from datetime import datetime
+from sl_model_functions import migrationsmodell_piringer, calculate_max_cp0, plot_results_area
 
 
 class SingleLayerTab(QWidget):
@@ -53,15 +56,17 @@ class SingleLayerTab(QWidget):
         self.sl_sub_tab_widget.addTab(sl_input_tab, "Eingabe")
 
     def create_calculation_tab(self):
-        """Erstellt den Berechnungs-Tab mit Platzhaltern für Ergebnisse."""
-        calculation_tab = QWidget()
-        calculation_tab_layout = QVBoxLayout(calculation_tab)
+        """Erstellt den Berechnungs-Tab mit Ergebnissen."""
+        self.calculation_tab = QWidget()
+        calculation_tab_layout = QVBoxLayout(self.calculation_tab)
 
-        # Platzhalter für Berechnungsergebnisse
-        calculation_tab_layout.addWidget(QLabel("Hier kommen die Berechnungsergebnisse hin."))
+        # Matplotlib Canvas für Plots
+        self.figure = Figure(figsize=(10, 6))
+        self.canvas = FigureCanvas(self.figure)
+        calculation_tab_layout.addWidget(self.canvas)
 
         # Tab hinzufügen
-        self.sl_sub_tab_widget.addTab(calculation_tab, "Berechnung")
+        self.sl_sub_tab_widget.addTab(self.calculation_tab, "Berechnung")
 
     def create_phy_chem_inputs(self):
         # Create layout for physical/chemical inputs
@@ -74,51 +79,52 @@ class SingleLayerTab(QWidget):
         form_layout.addRow(headline_label)  # Add headline directly to the form layout
 
         # Add input fields
-        T_C_input = QLineEdit()
-        t_max_input = QLineEdit()
-        M_r_input = QLineEdit()
-        c_P0_input = QLineEdit()
-        P_density_input = QLineEdit()
-        F_density_input = QLineEdit()
-        D_P_known_input = QLineEdit()
-        D_P_checkbox = QCheckBox("Bekannt")  # Checkbox to toggle D_P_known_input
-        K_PF_input = QLineEdit()
-        dt_input = QLineEdit()
+        self.T_C_input = QLineEdit()
+        self.t_max_input = QLineEdit()
+        self.M_r_input = QLineEdit()
+        self.c_P0_input = QLineEdit()
+        self.P_density_input = QLineEdit()
+        self.F_density_input = QLineEdit()
+        self.D_P_known_input = QLineEdit()
+        self.D_P_checkbox = QCheckBox("Bekannt")  # Checkbox to toggle D_P_known_input
+        self.K_PF_input = QLineEdit()
+        self.dt_input = QLineEdit()
 
         # Material dropdown
         material_list = ["LDPE", "LLDPE", "HDPE", "PP", "PET", "PS", "PEN", "HIPS"]
-        material_dropdown = QComboBox()
-        material_dropdown.addItems(material_list)      
+        self.material_dropdown = QComboBox()
+        self.material_dropdown.addItems(material_list)      
 
         # Narrow input fields
-        for input_field in [T_C_input, t_max_input, M_r_input, c_P0_input, P_density_input, F_density_input, D_P_known_input, K_PF_input, dt_input]:
+        for input_field in [self.T_C_input, self.t_max_input, self.M_r_input, self.c_P0_input, 
+                            self.P_density_input, self.F_density_input, self.D_P_known_input, self.K_PF_input, self.dt_input]:
             input_field.setMaximumWidth(70)  # Set max width to make fields narrower
         
         # First row of the form
-        form_layout.addRow(self._create_labeled_row("Material", "", material_dropdown))
+        form_layout.addRow(self._create_labeled_row("Material", "", self.material_dropdown))
         # Other rows of the form
-        form_layout.addRow(self._create_labeled_row("T<sub>C</sub>", "°C", T_C_input))
-        form_layout.addRow(self._create_labeled_row("t<sub>max</sub>", "s", t_max_input))
-        form_layout.addRow(self._create_labeled_row("Δt", "s", dt_input))
-        form_layout.addRow(self._create_labeled_row("M<sub>r</sub>", "g/mol", M_r_input))
-        form_layout.addRow(self._create_labeled_row("c<sub>P0</sub>", "mg/kg", c_P0_input))
-        form_layout.addRow(self._create_labeled_row("ρ<sub>P</sub>", "g/cm³", P_density_input))
-        form_layout.addRow(self._create_labeled_row("ρ<sub>F</sub>", "g/cm³", F_density_input))
-        form_layout.addRow(self._create_labeled_row("K<sub>PF</sub>", "-", K_PF_input))
+        form_layout.addRow(self._create_labeled_row("T<sub>C</sub>", "°C", self.T_C_input))
+        form_layout.addRow(self._create_labeled_row("t<sub>max</sub>", "s", self.t_max_input))
+        form_layout.addRow(self._create_labeled_row("Δt", "s", self.dt_input))
+        form_layout.addRow(self._create_labeled_row("M<sub>r</sub>", "g/mol", self.M_r_input))
+        form_layout.addRow(self._create_labeled_row("c<sub>P0</sub>", "mg/kg", self.c_P0_input))
+        form_layout.addRow(self._create_labeled_row("ρ<sub>P</sub>", "g/cm³", self.P_density_input))
+        form_layout.addRow(self._create_labeled_row("ρ<sub>F</sub>", "g/cm³", self.F_density_input))
+        form_layout.addRow(self._create_labeled_row("K<sub>PF</sub>", "-", self.K_PF_input))
         
         
         # Zeile für die Eingabe des Diffusionskoeffizienten mir "Checkbox toggle"
-        D_P_known_input.setEnabled(False)  # Default: disabled
+        self.D_P_known_input.setEnabled(False)  # Default: disabled
         
         def toggle_d_p_input(checked):
-            D_P_known_input.setEnabled(checked)
+            self.D_P_known_input.setEnabled(checked)
         
         # Signal verbinden
-        D_P_checkbox.toggled.connect(toggle_d_p_input)
+        self.D_P_checkbox.toggled.connect(toggle_d_p_input)
         
         # Add checkbox and input field for diffusion coefficient
-        D_P_row = self._create_labeled_row("D<sub>P</sub>", "cm²/s", D_P_known_input)
-        D_P_row.layout().insertWidget(3, D_P_checkbox)  # Add the checkbox in the row
+        D_P_row = self._create_labeled_row("D<sub>P</sub>", "cm²/s", self.D_P_known_input)
+        D_P_row.layout().insertWidget(3, self.D_P_checkbox)  # Add the checkbox in the row
         form_layout.addRow(D_P_row)
 
         # Tighten vertical spacing
@@ -143,32 +149,42 @@ class SingleLayerTab(QWidget):
         geo_layout.addWidget(headline_label)
         
         # Add input fields
-        d_P_input = QLineEdit()
-        d_F_input = QLineEdit()
-        V_P_input = QLineEdit()
-        V_F_input = QLineEdit()
-        A_PF_input = QLineEdit()
-        sim_case_dropdown = QComboBox()
+        self.d_P_input = QLineEdit()
+        self.d_F_input = QLineEdit()
+        self.V_P_input = QLineEdit()
+        self.V_F_input = QLineEdit()
+        self.A_PF_input = QLineEdit()
+        
+        # Signale verbinden, damit sich die Felder automatisch ausfüllen
+        self.d_P_input.textChanged.connect(self.update_geometric_inputs)
+        self.V_P_input.textChanged.connect(self.update_geometric_inputs)
+        self.d_F_input.textChanged.connect(self.update_geometric_inputs)
+        self.V_F_input.textChanged.connect(self.update_geometric_inputs)
+        self.A_PF_input.textChanged.connect(self.update_geometric_inputs)
+        
+        
+        self.sim_case_dropdown = QComboBox()
         simulation_case = ["worst","best"]
-        sim_case_dropdown.addItems(simulation_case)
-        sim_case_dropdown.setMaximumWidth(85)
+        self.sim_case_dropdown.addItems(simulation_case)
+        self.sim_case_dropdown.setMaximumWidth(85)
 
         # Narrow input fields
-        for input_field in [d_P_input, d_F_input, V_P_input, V_F_input, A_PF_input]:
+        for input_field in [self.d_P_input, self.d_F_input, self.V_P_input, self.V_F_input, self.A_PF_input]:
             input_field.setMaximumWidth(70)  # Set max width to make fields narrower
 
         # Create rows for combined inputs
         row_1_layout = QHBoxLayout()
-        row_1_layout.addWidget(self._create_labeled_row("d<sub>P</sub>", "cm", d_P_input))
-        row_1_layout.addWidget(self._create_labeled_row("d<sub>F</sub>", "cm", d_F_input))
+        row_1_layout.addWidget(self._create_labeled_row("A<sub>PF</sub>", "dm²", self.A_PF_input))
+        row_1_layout.addWidget(self._create_labeled_row("Simulation Case", "", self.sim_case_dropdown))
 
         row_2_layout = QHBoxLayout()
-        row_2_layout.addWidget(self._create_labeled_row("V<sub>P</sub>", "cm³", V_P_input))
-        row_2_layout.addWidget(self._create_labeled_row("V<sub>F</sub>", "cm³", V_F_input))
+        row_2_layout.addWidget(self._create_labeled_row("d<sub>P</sub>", "cm", self.d_P_input))
+        row_2_layout.addWidget(self._create_labeled_row("d<sub>F</sub>", "cm", self.d_F_input))
 
         row_3_layout = QHBoxLayout()
-        row_3_layout.addWidget(self._create_labeled_row("A<sub>PF</sub>", "dm²", A_PF_input))
-        row_3_layout.addWidget(self._create_labeled_row("Simulation Case", "", sim_case_dropdown))
+        row_3_layout.addWidget(self._create_labeled_row("V<sub>P</sub>", "cm³", self.V_P_input))
+        row_3_layout.addWidget(self._create_labeled_row("V<sub>F</sub>", "cm³", self.V_F_input))
+
 
         # Add rows to the form layout
         form_layout.addRow(row_1_layout)
@@ -182,6 +198,74 @@ class SingleLayerTab(QWidget):
         geo_layout.addLayout(form_layout)
 
         return geo_layout
+    
+    def update_geometric_inputs(self):
+        """Aktualisiert d_P, V_P, d_F und V_F dynamisch basierend auf Eingaben."""
+        try:
+            # Versuche, A_PF auszulesen
+            A_PF = float(self.A_PF_input.text()) if self.A_PF_input.text() else None
+
+            # Eingabewerte für d_P und V_P
+            d_P = float(self.d_P_input.text()) if self.d_P_input.text() else None
+            V_P = float(self.V_P_input.text()) if self.V_P_input.text() else None
+
+            # Eingabewerte für d_F und V_F
+            d_F = float(self.d_F_input.text()) if self.d_F_input.text() else None
+            V_F = float(self.V_F_input.text()) if self.V_F_input.text() else None
+
+            # Berechnungen nur durchführen, wenn A_PF vorhanden ist
+            if A_PF is not None:
+                # Berechnung für d_P und V_P
+                if self.sender() == self.d_P_input and d_P is not None:
+                    V_P = A_PF * d_P
+                    self.V_P_input.blockSignals(True)
+                    self.V_P_input.setText(f"{V_P:.4f}")
+                    self.V_P_input.blockSignals(False)
+                elif self.sender() == self.V_P_input and V_P is not None:
+                    d_P = V_P / A_PF
+                    self.d_P_input.blockSignals(True)
+                    self.d_P_input.setText(f"{d_P:.4f}")
+                    self.d_P_input.blockSignals(False)
+                elif self.sender() == self.A_PF_input:
+                    # A_PF wurde geändert, überprüfe und berechne neu
+                    if d_P is not None:
+                        V_P = A_PF * d_P
+                        self.V_P_input.blockSignals(True)
+                        self.V_P_input.setText(f"{V_P:.4f}")
+                        self.V_P_input.blockSignals(False)
+                    elif V_P is not None:
+                        d_P = V_P / A_PF
+                        self.d_P_input.blockSignals(True)
+                        self.d_P_input.setText(f"{d_P:.4f}")
+                        self.d_P_input.blockSignals(False)
+
+                # Berechnung für d_F und V_F
+                if self.sender() == self.d_F_input and d_F is not None:
+                    V_F = A_PF * d_F
+                    self.V_F_input.blockSignals(True)
+                    self.V_F_input.setText(f"{V_F:.4f}")
+                    self.V_F_input.blockSignals(False)
+                elif self.sender() == self.V_F_input and V_F is not None:
+                    d_F = V_F / A_PF
+                    self.d_F_input.blockSignals(True)
+                    self.d_F_input.setText(f"{d_F:.4f}")
+                    self.d_F_input.blockSignals(False)
+                elif self.sender() == self.A_PF_input:
+                    # A_PF wurde geändert, überprüfe und berechne neu
+                    if d_F is not None:
+                        V_F = A_PF * d_F
+                        self.V_F_input.blockSignals(True)
+                        self.V_F_input.setText(f"{V_F:.4f}")
+                        self.V_F_input.blockSignals(False)
+                    elif V_F is not None:
+                        d_F = V_F / A_PF
+                        self.d_F_input.blockSignals(True)
+                        self.d_F_input.setText(f"{d_F:.4f}")
+                        self.d_F_input.blockSignals(False)
+
+        except ValueError:
+            # Wenn ein Wert ungültig ist, überspringe die Berechnung
+            pass
 
     def create_grafical_setup(self):
         """Erstellt den Bereich für die grafische Darstellung."""
@@ -217,22 +301,68 @@ class SingleLayerTab(QWidget):
         layout.setAlignment(start_button, Qt.AlignRight)
         
         return layout
-    
-    # Hier weitermachen, Ich muss die Inputs als Instanzvariable definieren, damit ich von anderen funktionen darauf zugreifen kann. Momentan sind die nur lokal definiert. 
-#     also so: 
-#         # Add input fields
-# self.T_C_input = QLineEdit()
-# self.t_max_input = QLineEdit()
-# self.M_r_input = QLineEdit()
-# self.c_P0_input = QLineEdit()
-# self.P_density_input = QLineEdit()
-# self.F_density_input = QLineEdit()
-# self.D_P_known_input = QLineEdit()
-# self.K_PF_input = QLineEdit()
-# self.dt_input = QLineEdit()
+
+    def validate_inputs(self):
+        """Überprüft die Eingaben und markiert fehlerhafte Felder."""
+        is_valid = True
+
+        # Liste der Eingabefelder
+        all_fields = {
+            "T_C": self.T_C_input,
+            "t_max": self.t_max_input,
+            "M_r": self.M_r_input,
+            "c_P0": self.c_P0_input,
+            "P_density": self.P_density_input,
+            "F_density": self.F_density_input,
+            "D_P_known": self.D_P_known_input,
+            "K_PF": self.K_PF_input,
+            "dt": self.dt_input,
+            "d_P": self.d_P_input,
+            "V_P": self.V_P_input,
+            "d_F": self.d_F_input,
+            "V_F": self.V_F_input,
+            "A_PF": self.A_PF_input,
+        }
+
+        for field_name, field in all_fields.items():
+            if not self.is_valid_number(field.text()):
+                is_valid = False
+                self.mark_field_invalid(field)
+            else:
+                self.mark_field_valid(field)
+
+        return is_valid
+
+    def is_valid_number(self, value):
+        """Prüft, ob der Wert eine gültige Dezimalzahl ist."""
+        try:
+            float(value)
+            return True
+        except ValueError:
+            return False
+
+    def mark_field_invalid(self, field):
+        """Markiert ein Feld mit einer roten Umrandung, wenn es ungültig ist."""
+        field.setStyleSheet("border: 1px solid red;")
+
+    def mark_field_valid(self, field):
+        """Entfernt die rote Umrandung, wenn das Feld gültig ist."""
+        field.setStyleSheet("")
+
+    def show_error_message(self, message):
+        """Zeigt eine Fehlermeldung in der GUI an."""
+        error_label = QLabel(message)
+        error_label.setStyleSheet("color: red; font-weight: bold;")
+        self.main_layout.addWidget(error_label)
+
+
     def start_calculation(self):
-        print("Berechnung gestartet!")
         """Führt die Berechnung basierend auf den Eingaben durch."""
+        # Prüft ob Eingaben valide sind
+        if not self.validate_inputs():
+            self.show_error_message("Bitte korrigieren Sie die rot markierten Felder.")
+            return
+    
         # Physikalisch-chemische Eigenschaften
         M_r = float(self.M_r_input.text())
         T_C = float(self.T_C_input.text())
@@ -243,15 +373,15 @@ class SingleLayerTab(QWidget):
         D_P_known = None if not self.D_P_known_input.text() else float(self.D_P_known_input.text())
         K_PF = float(self.K_PF_input.text())
         t_max = float(self.t_max_input.text())
+        d_P = float(self.d_P_input.text())
+        V_P = float(self.V_P_input.text())
+        d_F = float(self.d_P_input.text())
+        V_F = float(self.V_P_input.text())
 
         # Geometrische Größen
-        d_P = float(self.d_P_input.text())
-        d_F = float(self.d_F_input.text())
-        V_P = float(self.V_P_input.text())  
-        V_F = float(self.V_F_input.text())
         A_PF = float(self.A_PF_input.text())
         
-        # Case
+        # Simulations-Case
         simulation_case = self.sim_case_dropdown.currentText()
 
         # Modellparameter nach Piringer
@@ -260,10 +390,24 @@ class SingleLayerTab(QWidget):
         # Berechnung der spez. Migrationsmenge
         results_area = migrationsmodell_piringer(M_r, T_C, c_P0, Material, P_density, F_density, K_PF, t_max, V_P, V_F, d_P, d_F, A_PF, dt, D_P_known, simulation_case)
 
-        # Ergebnisse in der Berechnungstabelle plotten
-        self.calculation_tab_layout.addWidget(QLabel("Ergebnisse der Berechnung:"))
-        for i, result in enumerate(results_area):
-            self.calculation_tab_layout.addWidget(QLabel(f"Zeitschritt {i}: {result}"))
+        self.plot_results_area(results_area, t_max, dt)
+
+    def plot_results_area(self, results_area, t_max, dt):
+        """Zeigt die berechneten Ergebnisse im Berechnungs-Tab als Plot an."""
+        self.figure.clear()  # Leere das aktuelle Plot-Fenster
+        ax = self.figure.add_subplot(111)
+
+        # Zeit in Tagen berechnen
+        time_days = np.arange(0, t_max / (3600 * 24), dt / (3600 * 24))
+        
+        # Plot der Ergebnisse
+        ax.plot(time_days, results_area, linewidth=2, color='#F06D1D')
+        ax.set_xlabel('Zeit $[Tage]$', fontsize=14)
+        ax.set_ylabel('spez. Migrationsmenge $[mg/dm^2]$', fontsize=14)
+        ax.tick_params(axis='both', which='major', labelsize=14)
+
+        # Plot aktualisieren
+        self.canvas.draw()
 
     def _create_labeled_row(self, label_text, unit_text, input_field):
             # Create a horizontal layout for the row
